e28c31b9108f4ccd91596914724f2eaf39868cd1
chrisguttandin <chrisguttandin@media-codings.com>
2016-09-10 01:00:54 +0200

    apply linting rules

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
                 'browserify',
                 'leche',
                 'mocha',
-                'sinon-chai' // implicitly uses chai too
+                'sinon-chai'
             ],
 
             preprocessors: {
