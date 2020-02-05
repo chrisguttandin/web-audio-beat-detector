a370b2d658c5d57663ffdd0bb2a339bd6bddf654
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-02-05 16:27:14 +0100

    build(config): transpile JavaScript to be compatible with ES2019
    BREAKING CHANGE: The JavaScript code linked by the module property is now compliant with ES2019. It was ES2018 before.

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -3,7 +3,7 @@ module.exports = {
         'clean:build',
         'webpack',
         'replace:worker',
-        'sh:build-es2018',
+        'sh:build-es2019',
         'sh:build-es5'
     ],
     lint: [
