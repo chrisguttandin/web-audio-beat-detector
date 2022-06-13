3cbe037628b17ed4de05a88638b69859578435c4
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-06-13 15:14:21 +0200

    build(config): use rimraf to delete build directory

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,5 +1,5 @@
 module.exports = {
-    build: ['clean:build', 'sh:webpack', 'sh:build-es2019', 'sh:build-es5'],
+    build: ['sh:clean', 'sh:webpack', 'sh:build-es2019', 'sh:build-es5'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
     test: ['build', 'sh:test-integration']
 };
